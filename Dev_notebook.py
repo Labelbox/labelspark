2422c894534ce5c4d242c36d648bd9842856bb9f
nlee@labelbox.com <nlee@labelbox.com>
2021-03-26 00:10:38 +0000

    Example API Call + Get API Key from Second Notebook
    Apologies for committing my API key to Github, but at least it's a private repo! Key will be deprecated in a few weeks.

--- Dev_notebook.py
+++ Dev_notebook.py
@@ -1,7 +1,20 @@
 # Databricks notebook source
 #this assumes you have another notebook called api_key and returns variable api_key 
 api_key = dbutils.notebook.run("api_key", 60)
-print(api_key)
+
+# COMMAND ----------
+
+from labelbox import Client
+
+if __name__ == '__main__':
+    API_KEY = api_key
+    client = Client(API_KEY)
+
+# COMMAND ----------
+
+projects = client.get_projects()
+for project in projects:
+    print(project.name, project.uid)
 
 # COMMAND ----------
 
---
+++ api_key.py
@@ -0,0 +1,8 @@
+# Databricks notebook source
+api_key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiJja21wamhhNWYzMW80MDc4OWRqbGNmM3JxIiwib3JnYW5pemF0aW9uSWQiOiJja2JpNmZhd21lYTJ1MDc0MHY4eHo0ZjFoIiwiYXBpS2V5SWQiOiJja21wamlxNXZoMWEzMDc0N3ZnZmJxeWxrIiwiaWF0IjoxNjE2NzE2OTM0LCJleHAiOjIyNDc4Njg5MzR9.ma0wwfeqF6K6DwAeTNZbmawAZSlmUzLFEbl4KNXjyVI"
+
+dbutils.notebook.exit(api_key)
+
+# COMMAND ----------
+
+
