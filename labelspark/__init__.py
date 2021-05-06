b4088377a177742e69a25a7367dc0e5c8bed7ae4
Nick Lee <nicklee@nicks-mbp.lan>
2021-05-06 09:27:29 -0700

    fixed formatting in init.py

--- labelspark/__init__.py
+++ labelspark/__init__.py
@@ -140,21 +140,20 @@ def dataframe_schema_enrichment(raw_dataframe, type_dictionary = None):
 
 	return copy_dataframe
 
-#helper code from https://pwsiegel.github.io/tech/nested-spark/
 def spark_schema_to_string(schema, progress=''):
 	if schema['type'] == 'struct':
 		for field in schema['fields']:
 			key = field['name']
 			yield from spark_schema_to_string(field, f'{progress}.{key}')
-    elif schema['type'] == 'array':
-        if type(schema['elementType']) == dict:
-            yield from spark_schema_to_string(schema['elementType'], progress)
-        else:
-            yield progress.strip('.')
-    elif type(schema['type']) == dict:
-        yield from spark_schema_to_string(schema['type'], progress)
-    else:
-        yield progress.strip('.')
+	elif schema['type'] == 'array':
+		if type(schema['elementType']) == dict:
+			yield from spark_schema_to_string(schema['elementType'], progress)
+		else:
+			yield progress.strip('.')
+	elif type(schema['type']) == dict:
+		yield from spark_schema_to_string(schema['type'], progress)
+	else:
+		yield progress.strip('.')
 
 
 def is_json(myjson):
