55c5c097530c3674abc192c13114ca20ea38bcfc
nlee@labelbox.com <nlee@labelbox.com>
2021-05-06 02:27:31 +0000

    Code is mostly done
    Final commit of the day

--- Labelspark_Demo_V3_Experimental.py
+++ Labelspark_Demo_V3_Experimental.py
@@ -162,10 +162,12 @@ print("Project Setup is complete.")
 #imports 
 from pyspark.sql.types import StructType    
 from pyspark.sql.functions import col
+from pyspark.sql import Row
+import ast
 
 # COMMAND ----------
 
-# DBTITLE 1,Query Labelbox for Bronze Annotation Table
+# DBTITLE 1,Query Labelbox for Raw Annotations (Bronze Table)
  def jsonToDataFrame(json, schema=None):
   #code taken from Databricks tutorial https://docs.azuredatabricks.net/_static/notebooks/transform-complex-data-types-python.html
   reader = spark.read
@@ -202,10 +204,11 @@ def get_annotations(client, project_id):
 
 if __name__ == '__main__':
     client = Client(API_KEY) #refresh client 
-    bronze_table = get_annotations(client,"ckoamhn1k5clr08584thrrp37")
+    bronze_table = get_annotations(client,"ckoc9avswe2tc08469c5sne5w")
     display(bronze_table)
     bronze_table.registerTempTable("movie_stills_demo")
     
+    #example of object and classification in same project: ckoc9avswe2tc08469c5sne5w
     #project = client.get_project("ckoaqvlhkcyqs0846kkfxtrqm")
     #project = client.get_project("ckoamhn1k5clr08584thrrp37") 
     #project = client.get_project("ckoaqvlhkcyqs0846kkfxtrqm") #inception 
@@ -232,7 +235,6 @@ if __name__ == '__main__':
 # COMMAND ----------
 
 # DBTITLE 1,Bronze Table II (Labels Flattened ) 
-from pyspark.sql.functions import col
 
 #helper code from https://pwsiegel.github.io/tech/nested-spark/
 def spark_schema_to_string(schema, progress=''):
@@ -261,13 +263,13 @@ def flatten_bronze_table(bronze_table):
       if success is not None: 
         valid_schemas.append(schema_field)
     except Exception as e:
-      print(e.__class__, "occurred for", schema_field, "as it is probably inside an array of JSONs")
+      #print(e.__class__, "occurred for", schema_field, "as it is probably inside an array of JSONs")
       schema_field_up_one_level = ".".join(schema_field.split(".")[:-1]) #very complicated way of popping the last hierarchy level 
       try:
         bronze_table.select(col(schema_field_up_one_level))
         if schema_field_up_one_level not in valid_schemas:
           valid_schemas.append(schema_field_up_one_level) 
-      except Exception as e: print(e.__class__, "occurred for", schema_field, "so I'm skipping it")
+      except Exception as e: pass #print(e.__class__, "occurred for", schema_field, "so I'm skipping it")
         
   bronze_table = bronze_table.select(*valid_schemas).toDF(*valid_schemas) 
   
@@ -280,31 +282,11 @@ if __name__ == '__main__':
     flattened_bronze_table = flatten_bronze_table(bronze_table)
     display(flattened_bronze_table)
 
-#     #Note that you cannot easily access some nested fields if you must navigate arrays of arrays to get there, so I do try/except to avoid parsing into those fields. I believe this can be enriched with some JSON parsing, but maybe another day. 
-#     valid_schemas = [] 
-#     for schema_field in schema_fields_array: 
-#       success = None
-#       try: 
-#         success = bronze_table.select(col(schema_field))
-#         if success is not None: 
-#           valid_schemas.append(schema_field)
-#       except Exception as e: 
-#         print(e.__class__, "occurred for", schema_field, "as it is probably inside an array of JSONs")
-#         schema_field_up_one_level = ".".join(schema_field.split(".")[:-1]) #very complicated way of popping the last hierarchy level 
-#         try: 
-#           bronze_table.select(col(schema_field_up_one_level))
-#           if schema_field_up_one_level not in valid_schemas: 
-#             valid_schemas.append(schema_field_up_one_level) 
-#         except Exception as e: print(e.__class__, "occurred for", schema_field, "so I'm skipping it")
-    
-#     bronze_table2 = bronze_table.select(*valid_schemas).toDF(*valid_schemas)
-#     display(bronze_table2) #the ultimate bronze table 
 
 # COMMAND ----------
 
 # DBTITLE 1,Silver Table Experimental
-from pyspark.sql import Row
-import ast
+
 
 def is_json(myjson):
   try:
@@ -319,27 +301,21 @@ def is_array_string(mystring): #a very quick and dirty way to see if this is an 
   else: return False 
   
 def add_json_answers_to_dictionary(title, answer2, my_dictionary): 
-  print(answer2)
-  try:
+  try: #see if I can read the answer string as a literal --it might be an array of JSONs
     convert_from_literal_string = ast.literal_eval(answer2)
-    print(type(convert_from_literal_string))
     if isinstance(convert_from_literal_string, list): 
       for item in convert_from_literal_string: #should handle multiple items
-        print(item)
-        my_dictionary = add_json_answers_to_dictionary(item["title"], item, my_dictionary) #recursive call 
+        my_dictionary = add_json_answers_to_dictionary(item["title"], item, my_dictionary) 
+        #recursive call to get into the array of arrays
   except Exception as e: 
     pass 
-  print(answer2)
+  
   if is_json(answer2): #sometimes the answer is a JSON string; this happens on project ckoamhn1k5clr08584thrrp37
-    print("answer is json")
     parsed_answer = json.loads(answer2)
-    print(parsed_answer)
     try: 
       my_dictionary[title] = parsed_answer["value"] #funky Labelbox syntax where the title is actually the "value" of the answer
     except Exception as e: 
-      pass#my_dictionary[title] = parsed_answer #just dump it in the dataframe and fix it later 
-#   elif isinstance(answer2, dict):
-#     my_dictionary[title] = answer2["title"]
+      pass
   else: 
     my_dictionary[title] = answer2
     
@@ -361,40 +337,25 @@ def bronze_to_silver(bronze_table):
       row["Label.classifications.title"]
       for i in range(len(row["Label.classifications.title"])): 
         title = row["Label.classifications.title"][i]
-        try: #this is for deeper nesting 
+        try: #this is for deeper nesting; sometimes this fails if there isn't more nesting, hence try except
           answer = row["Label.classifications.answer"][i]
         except Exception as e: 
           answer = row["Label.classifications.answer.title"][i]
         my_dictionary = add_json_answers_to_dictionary(title, answer, my_dictionary)
-        #my_dictionary[title] = answer 
     except Exception as e: 
-      print("not here")
+      print("No classifications") 
     
     my_dictionary["DataRowID"] = row.DataRowID #close it out 
     new_json.append(my_dictionary)
   
   parsed_classifications = pd.DataFrame(new_json).to_spark() #this is all leveraging Spark + Koalas! 
-  display(parsed_classifications)
   
-#   display(bronze_table)
-  #get objects fields to append 
-#   bronze_table = bronze_table.to_spark()
-#   object_columns = [col_name for col_name in bronze_table.columns if "Label.object" in col_name] + ["DataRowID"]
-#   #object_columns.append("DataRowID")
-#   print(object_columns)
-  
-#   object_df = bronze_table.select(col("Label.objects.bbox.height"))
-#   #display(object_df)
-  
-  #empDF.join(deptDF,empDF.emp_dept_id ==  deptDF.dept_id,"inner")
   bronze_table = bronze_table.to_spark()
   joined_df = parsed_classifications.join(bronze_table, ["DataRowID"], "inner")
   joined_df = joined_df.withColumnRenamed("DataRowID", "DataRow ID")
   
-  
   return joined_df #silver_table 
     
-
 if __name__ == '__main__':
     client = Client(API_KEY) #refresh client 
     silver_table = bronze_to_silver(bronze_table)
