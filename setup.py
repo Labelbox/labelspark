7b6c7716af54f06c87e0ea8e8db2630bd177c086
Nick Lee <nickaustinlee@gmail.com>
2021-05-26 15:42:15 -0700

    Update setup.py
    Added version and description to setup.py

--- setup.py
+++ setup.py
@@ -2,9 +2,10 @@ from setuptools import find_packages, setup
 
 setup(
     name='labelspark',
+    version='0.1.0',
     packages=find_packages(),
     url='https://github.com/Labelbox/LabelSpark.git',
-    description='Labelbox / Spark integration tools',
+    description='Labelbox & Databricks integration helper library',
     install_requires=[],
     extras_require={
         'dev': ['pylint']
