d7f49d3aab80e4445ce111b1272cd151e884434b
Matt Sokoloff <msokoloff@labelbox.com>
2021-06-24 18:24:41 -0400

    update readme and requirements

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     description='Labelbox & Databricks integration helper library',
     long_description=long_description,
     long_description_content_type="text/markdown",
-    install_requires=[],
+    install_requires=["labelbox"],
     extras_require={
         'dev': ['pylint']
     }
