424cc327b7db575addafaf7f9fc13c44d33429bf
Matt Sokoloff <msokoloff@labelbox.com>
2021-06-24 18:11:29 -0400

    add workflow, bump version, add long description

--- setup.py
+++ setup.py
@@ -1,11 +1,16 @@
 from setuptools import find_packages, setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name='labelspark',
-    version='0.1.0',
+    version='0.2.0',
     packages=find_packages(),
     url='https://github.com/Labelbox/LabelSpark.git',
     description='Labelbox & Databricks integration helper library',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     install_requires=[],
     extras_require={
         'dev': ['pylint']
