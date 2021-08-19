450265a509fe5589bd54904b3717d64c47a3faf2
Nick Lee <nicklee@nicks-mbp.lan>
2021-08-19 15:21:55 -0700

    pushing fixes to support dropdown and checklist classifications

--- setup.py
+++ setup.py
@@ -3,16 +3,12 @@ from setuptools import find_packages, setup
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-setup(
-    name='labelspark',
-    version='0.3.0',
-    packages=find_packages(),
-    url='https://github.com/Labelbox/LabelSpark.git',
-    description='Labelbox & Databricks integration helper library',
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    install_requires=["labelbox"],
-    extras_require={
-        'dev': ['pylint']
-    }
-)
+setup(name='labelspark',
+      version='0.4.0',
+      packages=find_packages(),
+      url='https://github.com/Labelbox/LabelSpark.git',
+      description='Labelbox & Databricks integration helper library',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      install_requires=["labelbox"],
+      extras_require={'dev': ['pylint']})
