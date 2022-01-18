0eecb319d46ee694caa51dda301d230ffc271b55
Cristobal Mitchell <cristobalmitchell@users.noreply.github.com>
2022-01-18 11:17:18 -0500

    Update __init__.py
    Corrections to accommodate backward compatibility with Databricks LTS 7.3 and greater 
    
    - os.environ.pop("ARROW_PRE_0_15_IPC_FORMAT") removes unnecessary environment variable if present. In scenarios where sc.version < 3.2.0 importing koalas will automatically set this variable (see for more details)

--- labelspark/__init__.py
+++ labelspark/__init__.py
@@ -1,5 +1,17 @@
 import json
 import urllib
+
+import os
+os.environ.pop("ARROW_PRE_0_15_IPC_FORMAT")
+
+from packaging import version
+if version.parse(spark.version) < version.parse("3.2.0"):
+  import databricks.koalas as pd 
+  needs_koalas = True  
+else:
+  import pyspark.pandas as pd
+  needs_koalas = False
+
 import ast
 from labelbox import Client
 
@@ -14,10 +26,13 @@ def create_dataset(client, spark_dataframe, dataset_name="Default"):
     # expects spark dataframe to have two columns: external_id, row_data
     # external_id is the asset name ex: "photo.jpg"
     # row_data is the URL to the asset
-    spark_dataframe = spark_dataframe.to_pandas_on_spark()
+    if needs_koalas:
+      spark_dataframe = spark_dataframe.to_koalas()
+    else:
+      spark_dataframe = spark_dataframe.to_pandas_on_spark()
     dataSet_new = client.create_dataset(name=dataset_name)
 
-    # ported Pandas code to Pandas-on-Spark
+    # ported Pandas code
     data_row_urls = [{
         "external_id": row['external_id'],
         "row_data": row['row_data']
@@ -46,12 +61,15 @@ def get_videoframe_annotations(bronze_video_labels, api_key, spark, sc):
     # an array of bronze dataframes containing frame labels for each project
     bronze_video_labels = bronze_video_labels.withColumnRenamed(
         "DataRow ID", "DataRowID")
-    pandas_bronze = bronze_video_labels.to_pandas_on_spark()
-
+    if needs_koalas:
+      bronze = bronze_video_labels.to_koalas()
+    else:
+      bronze = bronze_video_labels.to_pandas_on_spark()
+      
     # We manually build a string of frame responses to leverage our existing jsonToDataFrame code, which takes in JSON
     headers = {'Authorization': f"Bearer {api_key}"}
     master_array_of_json_arrays = []
-    for index, row in pandas_bronze.iterrows():
+    for index, row in bronze.iterrows():
         response = requests.get(row.Label.frames, headers=headers, stream=False)
         data = []
         for line in response.iter_lines():
@@ -112,8 +130,12 @@ def bronze_to_silver(bronze_table):
     if video:
         bronze_table = bronze_table.withColumnRenamed("Label.frameNumber",
                                                       "frameNumber")
-    bronze_table = bronze_table.to_pandas_on_spark()
+    if needs_koalas:
+      bronze_table = bronze_table.to_koalas()
+    else:
+      bronze_table = bronze_table.to_pandas_on_spark()
 
+      
     new_json = []
     for index, row in bronze_table.iterrows():
         my_dictionary = {}
@@ -152,7 +174,7 @@ def bronze_to_silver(bronze_table):
                 "frameNumber"] = row.frameNumber  # need to store the unique framenumber identifier for video
         new_json.append(my_dictionary)
 
-    parsed_classifications = spark.read.json(new_json)
+    parsed_classifications = pd.DataFrame(new_json).to_spark()
 
     bronze_table = bronze_table.to_spark()
     if video:
