7371519666994c723dca3bf8bf807c8eacfacc40
Nick Lee <nickaustinlee@gmail.com>
2022-01-26 16:14:22 -0800

    Update to 0.4.1 for hotfix

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(name='labelspark',
-      version='0.4.0',
+      version='0.4.1',
       packages=find_packages(),
       url='https://github.com/Labelbox/LabelSpark.git',
       description='Labelbox & Databricks integration helper library',
