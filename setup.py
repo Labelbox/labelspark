d1acb46f92eaa37bcbd976a5e7fe0b6956b42332
raphaeljafriLB <79109817+raphaeljafriLB@users.noreply.github.com>
2022-12-16 20:59:24 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(name='labelspark',
-      version='0.5.94',
+      version='0.5.941',
       packages=find_packages(),
       url='https://github.com/Labelbox/LabelSpark.git',
       description='Labelbox Connector for Databricks',
