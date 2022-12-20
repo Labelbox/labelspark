822384373cd1df436bf4b1262f83630f08f0244c
raphaeljafriLB <79109817+raphaeljafriLB@users.noreply.github.com>
2022-12-19 19:45:34 -0500

    Update connector.py

--- labelspark/connector.py
+++ labelspark/connector.py
@@ -240,9 +240,9 @@ def upsert_function(upsert_dict_bytes, global_key_col, metadata_value_col):
         return_value = metadata_value_col
     return return_value                
 
-def metadata_upsert_udf(**kwargs):
+def metadata_upsert_udf():
     """ Returns a UDF that will upsert a column given a dictionary where {key=column_name : value=new_value}
     Returns:
         UDF object to-be-run using: spark_dataframe.withColumn(col_name, udf(**kwargs))
     """      
-    return udf(upsert_function(**kwargs), StringType())
+    return udf(upsert_function(), StringType())
