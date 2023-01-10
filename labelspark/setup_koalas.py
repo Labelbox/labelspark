1e060c0329af356a2f80b5362fa634c0751ba0f0
raphaeljafriLB <79109817+raphaeljafriLB@users.noreply.github.com>
2023-01-10 13:48:56 -0500

    Delete setup_koalas.py

--- labelspark/setup_koalas.py
+++
@@ -1,6 +0,0 @@
-from pyspark import SparkContext
-from packaging import version
-
-def setup_koalas():
-    sc = SparkContext.getOrCreate()
-    return True if version.parse(sc.version) < version.parse("3.2.0") else False  
