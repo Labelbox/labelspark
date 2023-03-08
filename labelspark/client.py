106a2a08b080ea465856659538041aa8e5531eb4
raphaeljafriLB <79109817+raphaeljafriLB@users.noreply.github.com>
2023-03-08 12:21:22 -0500

    Update client.py

--- labelspark/client.py
+++ labelspark/client.py
@@ -1,11 +1,10 @@
+import pyspark
 from labelbox import Client as labelboxClient
-from labelbox.schema.dataset import Dataset as labelboxDataset
 from labelspark.uploader import create_upload_dict
 from labelspark.connector import check_pyspark, get_col_names, get_unique_values
 from labelbase.connector import validate_columns, determine_actions
 from labelbase.uploader import create_global_key_to_data_row_dict, batch_create_data_rows, batch_rows_to_project, batch_upload_annotations
 from labelbase.downloader import export_and_flatten_labels
-import pyspark
 
 class Client:
     """ A Databricks Client, containing a Labelbox Client, to-be-run a Databricks notebook
@@ -25,6 +24,36 @@ class Client:
     def __init__(self, lb_api_key=None, lb_endpoint='https://api.labelbox.com/graphql', lb_enable_experimental=False, lb_app_url="https://app.labelbox.com"):
         self.lb_client = labelboxClient(lb_api_key, endpoint=lb_endpoint, enable_experimental=lb_enable_experimental, app_url=lb_app_url)
         check_pyspark()
+        
+    def export_to_table(
+        self, project, 
+        include_metadata:bool=False, include_performance:bool=False, include_agreement:bool=False,
+        verbose:bool=False, mask_method:str="png", divider="///"):
+        """ Creates a Pandas DataFrame given a Labelbox Projet ID
+        Args:
+            project                 :   Required (str / lablebox.Project) - Labelbox Project ID or lablebox.Project object to export labels from
+            include_metadata        :   Optional (bool) - If included, exports metadata fields
+            include_performance     :   Optional (bool) - If included, exports labeling performance
+            include_agreement       :   Optional (bool) - If included, exports consensus scores       
+            verbose                 :   Optional (bool) - If True, prints details about code execution; if False, prints minimal information
+            mask_method             :   Optional (str) - Specifies your desired mask data format
+                                            - "url" leaves masks as-is
+                                            - "array" converts URLs to numpy arrays
+                                            - "png" converts URLs to png byte strings             
+            divider                 :   Optional (str) - String delimiter for schema name keys and suffix added to duplocate global keys 
+        """
+        flattened_labels_dict = export_and_flatten_labels(
+            client=self.lb_client, project=project, 
+            include_metadata=include_metadata, include_performance=include_performance, include_agreement=include_agreement,
+            mask_method=mask_method, verbose=verbose, divider=divider
+        )
+        
+        
+        
+        if verbose:
+            print(f"Success: DataFrame generated")
+        
+        return flattened_labels_dict        
     
     def create_data_rows_from_table(
         self, table:pyspark.sql.dataframe.DataFrame, dataset_id:str="", project_id:str="", priority:int=5, 
@@ -46,7 +75,6 @@ class Client:
             verbose             :   Optional (bool) - If True, prints details about code execution; if False, prints minimal information               
             divider             :   Optional (str) - String delimiter for schema name keys and suffix added to duplocate global keys
         """
-        check_pyspark()
         # Create/identify the following values:
             # row_data_col      : column with name "row_data"
             # global_key_col    : column with name "global_key" - defaults to row_data_col
@@ -158,149 +186,5 @@ class Client:
             "batch_to_project_results" : batch_to_project_results,
             "annotation_upload_results" : annotation_upload_results
         }
-
-    def create_table_from_dataset(self, lb_dataset:labelboxDataset, metadata_index:dict={}, verbose:bool=False, divider:str="///"):
-        """ Creates a Spark Table from a Labelbox dataset, optional metadata_fields will create 1 column per field name in list
-        Args:
-            lb_dataset      :   Required (labelbox.schema.dataset.Dataset) - Labelbox dataset to create a spark table from
-            metadata_index  :   Optional (dict) - Dictionary where {key=column_name : value=metadata_type}
-                                    metadata_type must be either "enum", "string", "datetime" or "number"
-            verbose         :   Required (bool) - If True, prints details about code execution; if False, prints minimal information
-            divider         :   Optional (str) - String delimiter for all name keys generated for parent/child schemas            
-        Returns:
-            Spark Table - pyspark.sql.dataframe.Dataframe objet with columns "data_row_id", "global_key", "external_id", "row_data" and optional metadata fields as columns
-        """        
-        table = sync_metadata_fields(
-            client=self.lb_client, table=False, get_columns_function=connector.get_columns_function, add_column_function=connector.add_column_function, 
-            get_unique_values_function=connector.get_unique_values_function, metadata_index=metadata_index, verbose=verbose
-        )                         
-        starttime = datetime.now()
-        metadata_fields = list(metadata_index.keys())
-        data_rows_list = list(lb_dataset.export_data_rows(include_metadata=True)) if metadata_fields else list(lb_dataset.export_data_rows(include_metadata=False))
-        endtime = datetime.now()
-        if verbose:
-            print(f'Labelbox data row export complete\n Start Time: {starttime}\n End Time: {endtime}\n Export Time: {endtime-starttime}\nData rows to create in table: {len(data_rows_list)}')
-        starttime = datetime.now()
-        metadata_schema_to_name_key = get_metadata_schema_to_name_key(client=self.lb_client)
-        # Create empty dict where {key= column name : value= list of values for column} (in order)
-        table_dict = {"data_row_id" : [], "global_key" : [], "external_id" : [], "row_data" : []}
-        if metadata_fields:
-            for column_name in metadata_fields:
-                table_dict[column_name] = []
-        # Append to your dict where {key= column name : value= list of values for column} (in order)
-        for data_row in data_rows_list:
-            # Create default column values
-            table_dict['data_row_id'].append(data_row.uid)
-            table_dict['global_key'].append(data_row.global_key)
-            table_dict['external_id'].append(data_row.external_id)
-            table_dict['row_data'].append(data_row.row_data)
-            # If there's metadata, add it, with default value as None
-            if metadata_fields:
-                metadata_dict = {metadata_name : None for metadata_name in metadata_fields}
-                if data_row.fields:
-                    for metadata_field in data_row.metadata_fields:
-                        if metadata_field['name'] in metadata_dict:
-                            # If a metatata value's string is a schemaId, it's an enum option we need to convert. Otherwise, we can take the value itself.
-                            metadata_dict[metadata_field['name']] = metadata_schema_to_name_key[metadata_field['value']].split("///")[1] if metadata_field['value'] in metadata_schema_to_name_key else metadata_field['value']
-                for metadata_name in metadata_dict:
-                    table_dict[metadata_name].append(metadata_dict[metadata_name])
-        table = pd.DataFrame.from_dict(table_dict).to_spark()
-        table = table.drop(col("lb_integration_source"))
-        endtime = datetime.now()
-        if verbose:
-            print(f'Success: Created table from Labelbox dataset with ID {lb_dataset.uid}\n Start Time: {starttime}\n End Time: {endtime}\n Total Time: {endtime-starttime}\nData rows in table: {len(data_rows_list)}')
-        return table
     
-    def upsert_table_metadata(self, table, global_key_col, global_keys_list=[], metadata_index={}, verbose:bool=False):
-        """ Upserts a Spark Table based on the most recent metadata in Labelbox, only upserts columns provided via metadata_fields list
-        Args:
-            table           :   Required (pyspark.sql.dataframe.DataFrame) - Spark Table
-            global_key_col  :   Required (str) - Column name containing the data row global key
-            global_keys_list:   Optional (list) - List of global keys you wish to upsert - defaults to the whole table
-            metadata_index  :   Optional (dict) - Dictionary where {key=column_name : value=metadata_type}
-                                    metadata_type must be either "enum", "string", "datetime" or "number"
-        Returns:
-            Upserted Spark table
-        """
-        # Export data row metadata from Labelbox through global keys
-        starttime = datetime.now()
-        lb_mdo = self.lb_client.get_data_row_metadata_ontology()
-        metadata_schema_to_name_key = get_metadata_schema_to_name_key(client=self.lb_client, lb_mdo=lb_mdo)
-        ## Either use global_keys provided or all the global keys in the provided global_key column 
-        global_keys = global_keys_list if global_keys_list else connector.get_unique_values(table, global_key_col)
-        ## Grab data row IDs with global_key list
-        data_row_ids = self.lb_client.get_data_row_ids_for_global_keys(global_keys)['results']
-        data_row_id_to_global_key = {str(data_row_ids[i]) : str(global_keys[i]) for i in range(0, len(data_row_ids))}
-        ## Get data row metadata with list of data row IDs
-        data_row_metadata = lb_mdo.bulk_export(data_row_ids)
-        endtime = datetime.now()
-        if verbose:
-            print(f'Labelbox metadata export complete\n Start Time: {starttime}\n End Time: {endtime}\n Export Time: {endtime-starttime}\nData rows in export: {len(data_row_metadata)}')        
-        # Run the table through a UDF that will update metadata column values using the Global Key as the primary key        
-        starttime = datetime.now()
-        ## Create a dict where {metadata_field_name (column name) : {global_key : new_metadata_value}} (for enums, is the schema ID)
-        metadata_fields = list(metadata_index.keys())
-        upsert_dict = {}
-        for data_row in data_row_metadata:
-            global_key = str(data_row_id_to_global_key[str(data_row.data_row_id)])
-            for field in data_row.fields:
-                if field.schema_id in metadata_schema_to_name_key:
-                    metadata_col = metadata_schema_to_name_key[field.schema_id]
-                    if metadata_col in metadata_fields:
-                        if metadata_col not in upsert_dict.keys():
-                            upsert_dict[metadata_col] = {}
-                        input_value = metadata_schema_to_name_key[field.value].split("///")[1] if field.value in metadata_schema_to_name_key.keys() else field.value
-                        upsert_dict[metadata_col][global_key] = input_value
-        ## For each metadata field column, use a UDF upsert column values with the values in your dict where {key=global_key : value=new_metadata_value}
-        upsert_udf = connector.column_upsert_udf()
-        for metadata_col in upsert_dict:
-            table = table.withColumn(metadata_col, upsert_udf(lit(json.dumps(upsert_dict[metadata_col])), global_key_col, metadata_col))
-        endtime = datetime.now()
-        if verbose:
-            print(f'Upsert table metadata complete\n Start Time: {starttime}\n End Time: {endtime}\n Total Time: {endtime-starttime}\nData rows upserted: {len(data_row_metadata)}') 
-        return spark_table
-
-    def upsert_labelbox_metadata(self, lb_client, spark_table, global_key_col, global_keys_list=[], metadata_index={}):
-        """ Updates Labelbox data row metadata based on the most recent metadata from a Databricks spark table, only updates metadata fields provided via a metadata_index keys
-        Args:
-            lb_client           :   Required( labelbox.client.Client) - Labelbox Client object                    
-            spark_table         :   Required (pyspark.sql.dataframe.Dataframe) - Spark Table
-            lb_dataset          :   Required (labelbox.schema.dataset.Dataset) - Labelbox dataset to add data rows to
-            global_key_col      :   Required (str) - Global key column name to map Labelbox data rows to the Databricks spark table rows
-            global_keys_list    :   Optional (list) - List of global keys you wish to upsert - defaults to the whole table
-            metadata_index      :   Optional (dict) - Determines what metadata gets upserted in Labelbox - dictionary where {key=column_name : value=metadata_type} - metadata_type must be one of "enum", "string", "datetime" or "number"
-        Returns:
-            List of errors from metadata ontology bulk upsert - if successful, is an empty list
-        """
-        # Export data row metadata from Labelbox through global keys        
-        starttime = datetime.now()
-        lb_mdo = self.lb_client.get_data_row_metadata_ontology()
-        metadata_schema_to_name_key = get_metadata_schema_to_name_key(client=self.lb_client, lb_mdo=lb_mdo)
-        metadata_name_key_to_schema = get_metadata_schema_to_name_key(client=self.lb_client, lb_mdo=lb_mdo, invert=True)
-        ## Either use global_keys provided or all the global keys in the provided global_key column 
-        global_keys = global_keys_list if global_keys_list else connector.get_unique_values(spark_table, global_key_col)
-        ## Grab data row IDs with global_key list
-        data_row_ids = self.lb_client.get_data_row_ids_for_global_keys(global_keys)['results']
-        data_row_id_to_global_key = {str(data_row_ids[i]) : str(global_keys[i]) for i in range(0, len(data_row_ids))}
-        ## Get data row metadata with list of data row IDs
-        data_row_metadata = lb_mdo.bulk_export(data_row_ids)  
-        endtime = datetime.now()
-        print(f'Laeblbox metadata export complete\n Start Time: {starttime}\n End Time: {endtime}\n Export Time: {endtime-starttime}\nData rows in export: {len(data_row_metadata)}')                
-        # Structure a new metadata upload by grabbing the latest values from your spark table, using the global key as the primary key
-        starttime = datetime.now()
-        metadata_fields = list(metadata_index.keys())
-        upload_metadata = []  
-        for data_row in data_row_metadata:
-            global_key = data_row_id_to_global_key[str(data_row.data_row_id)]
-            new_metadata = data_row.fields[:]
-            for field in new_metadata:
-                field_name = metadata_schema_to_name_key[field.schema_id]
-                if field_name in metadata_fields:
-                    table_value = spark_table.filter(f"{global_key_col} = '{global_key}'").collect()[0].__getitem__(field_name)
-                    table_name_key = f"{field_name}///{table_value}"
-                    field.value = metadata_name_key_to_schema[table_name_key] if table_name_key in metadata_name_key_to_schema.keys() else table_value
-            upload_metadata.append(DataRowMetadata(data_row_id=data_row.data_row_id, fields=new_metadata))
-        results = lb_mdo.bulk_upsert(upload_metadata)
-        endtime = datetime.now()
-        print(f'Upsert Labelbox metadata complete\n Start Time: {starttime}\n End Time: {endtime}\n Total Time: {endtime-starttime}\nData rows upserted: {len(upload_metadata)}')         
-        return results
+    
