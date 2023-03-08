ba94bcaecaed0448a818d7d228019a7906cea3a6
raphaeljafriLB <79109817+raphaeljafriLB@users.noreply.github.com>
2023-03-08 12:00:24 -0500

    Update connector.py

--- labelspark/connector.py
+++ labelspark/connector.py
@@ -11,12 +11,6 @@ connector.py holds the following helper functions specific to pandas DataFrames:
 
 import pyspark
 from labelbase.connector import validate_column_name_change
-from pyspark.sql.functions import udf, lit
-from labelbox.schema.data_row_metadata import DataRowMetadataKind
-from labelbox import Client as labelboxClient
-from labelbase.metadata import get_metadata_schema_to_name_key
-from pyspark.sql.types import StructType, StructField, StringType, MapType, ArrayType
-import json
 
 def check_pyspark():
     try:
@@ -33,7 +27,6 @@ def get_col_names(table:pyspark.sql.dataframe.DataFrame, extra_client=None):
         List of strings corresponding to all column names
     """
     return [str(col[0]) for col in table.dtypes]    
-
 
 def get_unique_values(table:pyspark.sql.dataframe.DataFrame, col:str, extra_client=None):
     """ Grabs all unique values from a Spark Dataframe column as strings
@@ -67,9 +60,9 @@ def get_table_length(table:pyspark.sql.dataframe.DataFrame, extra_client=None):
     Returns:
         The length of your table as an integer
     """  
-    return len(table)
+    return table.count()
 
-def rename_col(table: pandas.core.frame.DataFrame, col:str, to:str):
+def rename_col(table:pyspark.sql.dataframe.DataFrame, col:str, to:str):
     """ Renames columns into the Labelspark format    
     Args:
         table           :   Required (pyspark.sql.dataframe.DataFrame) - Spark Table
@@ -78,24 +71,6 @@ def rename_col(table: pandas.core.frame.DataFrame, col:str, to:str):
     Returns:
         Updated `table` object with renamed column
     """
-    existing_cols = get_col_names(table, extra_client=False)
-    validate_column_name_change(old_col_name=col, new_col_name=to, existing_col_names=existing_cols)
-    table = table.rename(columns={col:to})
+    table = table.withColumnRenamed(col,to)
     return table                  
-  
-def upsert_function(upsert_dict_bytes, global_key_col, metadata_value_col):
-    """ Nested UDF Functionality to upsert a column in a Databricks Spark table given a dictionary where 
-    Args:  
-        upsert_dict_bytes               :   Required (bytes) - Bytearray representation of a dictionary where {key=global_key : value=new_value}
-        global_key_col                  :   Required (str) - Column name for the global_key
-        metadata_value_col              :   Required (str) - Target column name
-    Returns:
-        New value to-be-inserted in the column corresponding to this metadata field
-    """  
-    upsert_dict = json.loads(upsert_dict_bytes)
-    if global_key_col in upsert_dict.keys():
-        return_value = upsert_dict[global_key_col]
-    else:
-        return_value = metadata_value_col
-    return return_value
-
+ 
