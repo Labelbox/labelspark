2db3a1c6de986b5f03d7fe7cb8191c908abf6497
raphaeljafriLB <79109817+raphaeljafriLB@users.noreply.github.com>
2023-03-13 16:57:42 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,12 +3,16 @@ from setuptools import find_packages, setup
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-setup(name='labelspark',
-      version='0.7.27',
-      packages=find_packages(),
-      url='https://github.com/Labelbox/LabelSpark.git',
-      description='Labelbox Connector for Databricks',
-      long_description=long_description,
-      long_description_content_type="text/markdown",
-      install_requires=["labelbox[data]", "pyspark", "packaging", "labelbase"],
-      extras_require={'dev': ['pylint']})
+setup(
+    name='labelspark',
+    version='0.7.27',
+    author='Labelbox',
+    author_email="raphael@labelbox.com",      
+    packages=find_packages(),
+    url='https://github.com/Labelbox/LabelSpark.git',
+    description='Labelbox Connector for Databricks',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    install_requires=["labelbox[data]", "pyspark", "packaging", "labelbase"],
+    extras_require={'dev': ['pylint']}
+)
