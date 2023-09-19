1669211121c6b426cb3c5dd5b8bd1a12c8587eef
raphaeljafriLB <79109817+raphaeljafriLB@users.noreply.github.com>
2023-09-19 14:03:44 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='labelspark',
-    version='0.7.28',
+    version='0.7.34',
     author='Labelbox',
     author_email="raphael@labelbox.com",      
     packages=find_packages(),
